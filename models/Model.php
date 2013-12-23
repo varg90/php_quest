17a08333681622cda4304902423d053948fcc44f
akun <akun@issart.com>
2013-12-23 19:54:10 +0800

    db config to ini file

--- models/Model.php
+++ models/Model.php
@@ -2,15 +2,11 @@
 class Model
 {
     protected $_db;
-    private $_config = array(
-        'host' => '127.0.0.1',
-        'db' => 'php_quest',
-        'user' => 'root',
-        'password' => ''
-    );
+    private $_config;
 
     public function __construct()
     {
+        $this->_config = parse_ini_file('config/db.ini');
         $this->_db = new PDO(
             'mysql:host=' . $this->_config['host'] . ';dbname=' . $this->_config['db'],
             $this->_config['user'],
